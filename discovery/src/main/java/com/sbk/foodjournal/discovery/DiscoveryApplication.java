3879dec4429146e7bf7510e31d1d697a70a9e716
SergiySobolev <sergiy.sobolev@gmail.com>
2016-05-08 01:37:28 +0300

    EnabledEurekaServer, properties

--- discovery/src/main/java/com/sbk/foodjournal/discovery/DiscoveryApplication.java
+++ discovery/src/main/java/com/sbk/foodjournal/discovery/DiscoveryApplication.java
@@ -2,11 +2,13 @@ package com.sbk.foodjournal.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * Created by plaguedocktor on 07.05.2016.
  */
 @SpringBootApplication
+@EnableEurekaServer
 public class DiscoveryApplication {
     public static void main(String[] args) {
         SpringApplication.run(DiscoveryApplication.class, args);
