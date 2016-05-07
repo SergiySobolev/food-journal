bb8b282d861e2af0b02924790e2a68fd6fb5649f
SergiySobolev <sergiy.sobolev@gmail.com>
2016-05-07 16:02:56 +0300

    Initial Build

---
+++ discovery/src/main/java/com/sbk/foodjournal/discovery/DiscoveryApplication.java
@@ -0,0 +1,14 @@
+package com.sbk.foodjournal.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by plaguedocktor on 07.05.2016.
+ */
+@SpringBootApplication
+public class DiscoveryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryApplication.class, args);
+    }
+}
